f9d95337cd7ec4371240bb6f44da4c5a26676da9
Damo <890601n14@gmail.com>
2023-09-12 10:39:09 +0800

    make it better

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def handle_message(event):
     get_message = event.message.text
 
     # Send To Line
-    reply = TextSendMessage(text=f"{get_message}1324")
+    reply = TextSendMessage(text=f"{get_message}132")
     line_bot_api.reply_message(event.reply_token, reply)
