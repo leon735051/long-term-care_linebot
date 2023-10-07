ada341bbad2119f9bf4016ae635f686c2ea860ca
Damo <890601n14@gmail.com>
2023-10-07 10:09:55 +0800

    make it better

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def callback():
         return "OK"
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+async def handle_message(event):
     user_id = event.source.user_id
     get_message = event.message.text
 
